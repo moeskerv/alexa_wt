1f52f0f7a809dd9decea25f2a20153f5eaec12b0
volker <15142022+moeskerv@users.noreply.github.com>
2020-07-26 16:53:30 +0200

    code cleanup only prompt active pitmaster adde support for - no temperature channels active - no current data available - 204 -> no data available - 412 -> no account linked (in progress) - general error on unhandled return code
    Signed-off-by: volker <15142022+moeskerv@users.noreply.github.com>

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,7 @@ Dependencies:
 
 TODOs
 
-- support HTTP 204 - empty data
+- support HTTP 204 - empty data -> DONE
 - support HTTP 412 - device - alexa connection missing, echo out code
 - nicen cards, add line breaks -> DONE
 - use POST request to get data
@@ -18,6 +18,8 @@ TODOs
 - align print and logger debug output -> DONE
 - implement unit test against static mock in WLANThermo cloud
 - make better use of echo displays (e.g. WLANThermo logo in background)
+- check data age and prompt if data is too old -> DONE (untested)
+- only prompt pitmaster which are active -> DONE (untested)
 
 Links:
 
@@ -29,6 +31,7 @@ https://developer.amazon.com/en-US/docs/alexa/custom-skills/request-and-response
 import json
 import requests
 import logging
+import time
 
 # init logger
 logger = logging.getLogger()
@@ -40,11 +43,14 @@ VERSION = '1.0.0'
 USERAGENT = 'WlanThermoAlexaSkill/' + VERSION
 
 # errorcodes
-NO_ERROR = 0
-INVALID_AUTHORIZATION_CREDENTIAL = 1
-INTERNAL_ERROR = 2
-ACCOUNT_LINKING_MISSING = 3
-NO_DATA_AVAILABLE = 4
+NO_ERROR = 200
+NO_DATA_AVAILABLE = 204
+INVALID_AUTHORIZATION_CREDENTIAL = 401
+INTERNAL_ERROR = 500
+ACCOUNT_LINKING_MISSING = 412
+
+# Misc constants
+DATA_MAX_AGE = 15 * 60  # 15 Minutes max data age
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -84,7 +90,7 @@ def get_wlanthermo_data(user_id):
     BASEURL = "https://dev-cloud.wlanthermo.de/"
     API_TOKEN = "2462abc331700b5426a258"
 
-    errorOccurred = NO_ERROR
+    request_status = NO_ERROR
     url = BASEURL + "data.php?api_token=" + API_TOKEN
 
     # adjust the user agent used in this request
@@ -94,29 +100,20 @@ def get_wlanthermo_data(user_id):
 
     try:
         response = requests.get(url, headers=headers)
+        request_status = response.status_code
 
-        if (response.status_code == 200):
+        if request_status == NO_ERROR or request_status == ACCOUNT_LINKING_MISSING:
             dataJson = response.json()
-
-        elif (response.status_code == 204):
-            errorOccurred = NO_DATA_AVAILABLE
-
-        elif (response.status_code == 412):
-            errorOccurred = ACCOUNT_LINKING_MISSING
-            dataJson = response.json()
-
-        else:  # everything else goes here
-            errorOccurred = INTERNAL_ERROR
 
     except requests.exceptions.Timeout:
-        errorOccurred = INTERNAL_ERROR
+        request_status = INTERNAL_ERROR
         logger.info("Send request: Timeout detected")
 
     except:
         logger.exception('Send request: Unknown Error occurred')
-        errorOccurred = INTERNAL_ERROR
+        request_status = INTERNAL_ERROR
 
-    return errorOccurred, dataJson
+    return request_status, dataJson
 
 
 # --------------- Functions that control the skill's behavior ------------------
@@ -140,19 +137,26 @@ def get_welcome_response():
 def get_temperatures(wlanthermo_data):
 
     session_attributes = {}
-    card_title = "Temperaturen"
-    card_text = "Temperatur Ist-Werte: \n"
-    speech_output = "Die aktuellen Temperaturen der aktiven Fühler sind: "
     reprompt_text = ""
     should_end_session = False
+    card_title = "Temperaturen"
+    active_channel_count = 0
+
+    card_text = "Temperatur Ist-Werte: \n"
+    speech_output = "Die Temperaturen der aktiven Fühler sind: "
 
     # iterate through all temps, take those which are != 999
     for channel in wlanthermo_data['channel']:
         if channel['temp'] != 999:
+            active_channel_count += 1
             channel_text = channel['name'] + ": " + \
                 str(channel['temp']).replace('.', ',') + " Grad "
             speech_output = speech_output + channel_text
             card_text = card_text + channel_text + "\n"
+
+    if active_channel_count == 0:
+        card_text = "Es ist kein Temperaturfühler aktiv"
+        speech_output = card_text
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, card_text, speech_output, reprompt_text, should_end_session))
@@ -166,13 +170,21 @@ def get_pitmaster(wlanthermo_data):
     speech_output = "Die aktuellen Werte der Pitmaster sind: "
     reprompt_text = ""
     should_end_session = False
+    active_channel_count = 0
 
     # iterate through all temps, take those which are != 999
     for pitmaster in wlanthermo_data['pitmaster']:
-        channel_text = "Pitmaster" + \
-            str(pitmaster['channel']) + ": " + str(pitmaster['value']) + " % "
-        speech_output = speech_output + channel_text
-        card_text = card_text + channel_text + "\n"
+        if channel['typ'] != "off":
+            active_channel_count += 1
+            channel_text = "Pitmaster" + \
+                str(pitmaster['channel']) + ": " + \
+                str(pitmaster['value']) + " % "
+            speech_output = speech_output + channel_text
+            card_text = card_text + channel_text + "\n"
+
+    if active_channel_count == 0:
+        card_text = "Es ist kein Pitmaster aktiv"
+        speech_output = card_text
 
     return build_response(session_attributes, build_speechlet_response(
         card_title, card_text, speech_output, reprompt_text, should_end_session))
@@ -190,20 +202,63 @@ def get_battery(wlanthermo_data):
         card_title, speech_output, speech_output, reprompt_text, should_end_session))
 
 
+def handle_204():
+
+    session_attributes = {}
+    reprompt_text = ""
+    should_end_session = True
+    card_title = "Keine Daten verfügbar."
+
+    speech_output = "Es liegen keine aktuellen Daten vor."
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, speech_output, reprompt_text, should_end_session))
+
+
+def handle_412(wlanthermo_data):
+
+    session_attributes = {}
+    reprompt_text = ""
+    should_end_session = True
+    token = "12345678abcdef"
+    card_title = "WlanThermo noch nicht verbunden."
+    card_text = "WLANThermo Verbindung mit Alexa wurde noch nicht konfiguriert.\n"\
+        "Bitte gib folgenden Token in der WlanThermo Cloud ein, \n"\
+        "um die Verbindung herzustellen: " + token
+
+    speech_output = "WLANThermo Verbindung mit Alexa wurde noch nicht konfiguriert."\
+        "Bitte gib folgenden Token in der WlanThermo Cloud ein, "\
+        "um die Verbindung herzustellen: " + token
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, card_text, speech_output, reprompt_text, should_end_session))
+
+
+def handle_error(request_status):
+
+    session_attributes = {}
+    reprompt_text = ""
+    should_end_session = True
+
+    speech_output = "Es ist ein Fehler aufgetreten, versuche es später noch einmal. Fehlercode: " + \
+        str(request_status)
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, speech_output, reprompt_text, should_end_session))
+
+
 def handle_session_end_request():
     card_title = "Auf Wiedersehen!"
     speech_output = "Vielen Dank für die Nutzung des WlanThermo Skills. "
-    # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
-
+        card_title, speech_output, speech_output, None, should_end_session))
 
 # --------------- Events ------------------
 
+
 def on_session_started(session_started_request, session):
     """ Called when the session starts """
-
     logger.info("on_session_started requestId=" + session_started_request['requestId']
                 + ", sessionId=" + session['sessionId'])
 
@@ -227,23 +282,39 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # fetch data from cloud
-    errorOccured, wlanthermo_data = get_wlanthermo_data(
+    request_status, wlanthermo_data = get_wlanthermo_data(
         session['user']['userId'])
     logger.info("Response: " + json.dumps(wlanthermo_data))
 
-    # Dispatch to your skill's intent handlers
-    if intent_name == "GetTemperatures":
-        return get_temperatures(wlanthermo_data)
-    elif intent_name == "GetPitmaster":
-        return get_pitmaster(wlanthermo_data)
-    elif intent_name == "GetBattery":
-        return get_battery(wlanthermo_data)
-    elif intent_name == "AMAZON.HelpIntent" or intent_name == "AMAZON.NavigateHomeIntent":
-        return get_welcome_response()
-    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-        return handle_session_end_request()
-    else:
-        raise ValueError("Invalid intent")
+    # check age of data, if too old, simulate 204 -> no data
+    if request_status == 200:
+        if time.time() - int(wlanthermo_data['system']['time']) > DATA_MAX_AGE:
+            request_status = 204
+
+    if request_status == 200:  # Dispatch skill's intent handlers
+
+        if intent_name == "GetTemperatures":
+            return get_temperatures(wlanthermo_data)
+        elif intent_name == "GetPitmaster":
+            return get_pitmaster(wlanthermo_data)
+        elif intent_name == "GetBattery":
+            return get_battery(wlanthermo_data)
+        elif intent_name == "AMAZON.HelpIntent" or intent_name == "AMAZON.NavigateHomeIntent":
+            return get_welcome_response()
+        elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+            return handle_session_end_request()
+        else:
+            raise ValueError("Invalid intent")
+
+    # no data available (last data older than 48h or no data yet)
+    elif request_status == 204:
+        return handle_204()
+
+    elif request_status == 412:  # precondition failed -> no link between alexa and wlanthermo
+        return handle_412(wlanthermo_data)
+
+    else:  # unhandled return code
+        return handle_error(request_status)
 
 
 def on_session_ended(session_ended_request, session):
